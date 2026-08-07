b4a240dd3ea27caef70c41b8f3ee9ad1bd3a019d
najibullah-platforms <najibullah.platforms@gmail.com>
2026-08-07 16:01:25 +0500

    Add Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,23 @@ import WhatsAppButton from "./components/WhatsAppButton/WhatsAppButton";
 import NotFound from "./pages/NotFound/NotFound";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+
 function App() {
   return (
-     <>
+    <>
       <ScrollToTop />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/products/:id" element={<ProductDetails />} />
-      <Route path="/faq" element={<FAQ />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NotFound />} />
 
-    </Routes>
-    <WhatsAppButton />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <WhatsAppButton />
     </>
   );
 }
---
+++ src/analytics/analytics.js
@@ -0,0 +1,12 @@
+import ReactGA from "react-ga4";
+
+export const initGA = () => {
+  ReactGA.initialize("G-CPTLXDMFG9");
+};
+
+export const trackPageView = (path) => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: path,
+  });
+};
